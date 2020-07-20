90899224d4e9db169aceba3f815b48c1a55afcd4
akter_rakib <rakibccj@gmail.com>
2020-07-21 00:10:25 +0600

    project initiate

---
+++ src/main/java/com/vhaibrother/vehicle_client_mng/VehicleClientMngApplication.java
@@ -0,0 +1,13 @@
+package com.vhaibrother.vehicle_client_mng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleClientMngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleClientMngApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vhaibrother/vehicle_client_mng/VehicleClientMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhaibrother.vehicle_client_mng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleClientMngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
