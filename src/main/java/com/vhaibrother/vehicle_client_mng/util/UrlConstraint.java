e2f4c9e88a2c646db7e249dd4aa3f6f3b5778019
atuar123 <atuar.cse@gmail.com>
2020-09-22 15:35:15 +0600

    Changed url format at UrlConstraint

--- src/main/java/com/vhaibrother/vehicle_client_mng/util/UrlConstraint.java
+++ src/main/java/com/vhaibrother/vehicle_client_mng/util/UrlConstraint.java
@@ -10,14 +10,14 @@ public final class UrlConstraint {
     private static final String API = "/api";
 
     public static class ColorManagement {
-        public static final String ROOT = API + VERSION + "/colors";
+        public static final String ROOT = VERSION + API + "/colors";
         public static final String DELETE = "/{colorId}";
         public static final String GET = "/{colorId}";
         public static final String PUT = "/{colorId}";
     }
 
     public static class DomainManagement {
-        public static final String ROOT =  API + VERSION + "/domains";
+        public static final String ROOT =  VERSION + API + "/domains";
         public static final String DELETE = "/{domainId}";
         public static final String GET = "/{domainId}";
         public static final String PUT = "/{domainId}";
@@ -42,19 +42,19 @@ public final class UrlConstraint {
         public static final String LOGIN = "/login";
     }
     public static class ProfessionManagement{
-        public static final String ROOT =  API + VERSION + "/professions";
+        public static final String ROOT =  VERSION + API + "/professions";
         public static final String DELETE = "/{professionId}";
         public static final String GET = "/{professionId}";
         public static final String PUT = "/{professionId}";
     }
     public static class ClientManagement{
-        public static final String ROOT = API + VERSION + "/clients";
+        public static final String ROOT = VERSION + API +  "/clients";
         public static final String DELETE = "/{clientId}";
         public static final String GET = "/{clientId}";
         public static final String PUT = "/{clientId}";
     }
     public static class CarGradeManagement{
-        public static final String ROOT = API + VERSION + "/carGrades";
+        public static final String ROOT = VERSION + API + "/carGrades";
         public static final String DELETE = "/{carGradeId}";
         public static final String GET = "/{carGradeId}";
         public static final String PUT = "/{carGradeId}";
