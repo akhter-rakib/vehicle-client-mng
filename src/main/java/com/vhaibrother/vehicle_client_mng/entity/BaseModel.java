4768be6c07e5ad653691ae5dfca55426110c8630
nedhro <nedhro786>
2020-11-19 22:51:46 +0600

    Base model activeStatus updated for edit and delete

--- src/main/java/com/vhaibrother/vehicle_client_mng/entity/BaseModel.java
+++ src/main/java/com/vhaibrother/vehicle_client_mng/entity/BaseModel.java
@@ -33,8 +33,13 @@ public class BaseModel implements Serializable {
     @PreUpdate
     public void setPreUpdateData() {
         this.updateAt = new Date();
-        if (this.activeStatus != ActiveStatus.DELETE.getValue())
+        if(this.activeStatus == null) {
             this.activeStatus = ActiveStatus.ACTIVE.getValue();
+        }
+        if (this.activeStatus != ActiveStatus.DELETE.getValue()) {
+            this.activeStatus = ActiveStatus.ACTIVE.getValue();
+        }
+
     }
 
 }
